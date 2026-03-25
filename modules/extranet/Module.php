cc6d6c4c9ba0548162739af191aabe6f9bccc0da
lumierer-MULAGWA <21mb358@esisalama.org>
2026-03-25 16:52:18 +0200

    modules generator

---
+++ modules/extranet/Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\modules\extranet;
+
+/**
+ * extranet module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'app\modules\extranet\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
