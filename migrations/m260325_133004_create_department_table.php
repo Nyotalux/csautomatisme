20e61e83f2f729773e303113dc7c116756c98e44
lumierer-MULAGWA <21mb358@esisalama.org>
2026-03-25 15:31:34 +0200

    Premier commit : Structure Yii2 et modules

---
+++ migrations/m260325_133004_create_department_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%department}}`.
+ */
+class m260325_133004_create_department_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%department}}', [
+            'id' => $this->primaryKey(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%department}}');
+    }
+}
