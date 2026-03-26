b2690e312ec8e46f4fe2c5e547aa0ed7228c579e
lumiere-MULAGWA <21mb358@esisalama.org>
2026-03-26 18:04:15 +0200

    add view extranet

--- modules/extranet/views/default/index.php
+++ modules/extranet/views/default/index.php
@@ -1,12 +1,46 @@
+<?php
+$this->title = 'Extranet - Administration';
+?>
 <div class="extranet-default-index">
-    <h1><?= $this->context->action->uniqueId ?></h1>
-    <p>
-        This is the view content for action "<?= $this->context->action->id ?>".
-        The action belongs to the controller "<?= get_class($this->context) ?>"
-        in the "<?= $this->context->module->id ?>" module.
-    </p>
-    <p>
-        You may customize this page by editing the following file:<br>
-        <code><?= __FILE__ ?></code>
-    </p>
+    <h1>Bienvenue dans l'extranet</h1>
+    <p>Gérez vos départements, services et secteurs d'intervention.</p>
+    
+    <div class="row mt-4">
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Départements</h5>
+                    <p class="card-text">Gérer les départements</p>
+                    <a href="<?= \yii\helpers\Url::to(['/extranet/department']) ?>" class="btn btn-primary">Accéder</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Services</h5>
+                    <p class="card-text">Gérer les services</p>
+                    <a href="<?= \yii\helpers\Url::to(['/extranet/service']) ?>" class="btn btn-primary">Accéder</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Secteurs</h5>
+                    <p class="card-text">Gérer les secteurs d'intervention</p>
+                    <a href="<?= \yii\helpers\Url::to(['/extranet/sector']) ?>" class="btn btn-primary">Accéder</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Galerie</h5>
+                    <p class="card-text">Gérer les images</p>
+                    <a href="<?= \yii\helpers\Url::to(['/extranet/gallery']) ?>" class="btn btn-primary">Accéder</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
